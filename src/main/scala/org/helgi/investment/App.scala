8846a18937c5ca6b22ea0105e42a3db8cfeca714
Oleh Kir <oleggio.kir@gmail.com>
2022-05-28 19:28:48 +0300

    Small clean up

--- src/main/scala/org/helgi/investment/App.scala
+++ src/main/scala/org/helgi/investment/App.scala
@@ -4,7 +4,7 @@ import cats.effect.{ExitCode, IO, IOApp}
 
 
 object App extends IOApp :
-  def run(args: List[String]): IO[ExitCode] =
+  override def run(args: List[String]): IO[ExitCode] =
     Server.create[IO]
       .use(_ => IO.never)
       .as(ExitCode.Success)
